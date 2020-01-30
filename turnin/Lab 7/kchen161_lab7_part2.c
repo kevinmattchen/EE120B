33f939e3a203295408ffc43e0b9e25aa31b1a6cb
Kevin Chen <kevinmattchen@gmail.com>
2020-01-30 14:00:20 -0800

    Updated header

--- turnin/Lab 7/kchen161_lab7_part2.c
+++ turnin/Lab 7/kchen161_lab7_part2.c
@@ -1,7 +1,7 @@
 /*	Author: kchen161
  *      Partner(s) Name: Kevin Chen
  *	Lab Section: 23
- *	Assignment: Lab #6  Exercise 2
+ *	Assignment: Lab #7  Exercise 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
