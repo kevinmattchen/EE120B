5bd548073cec8b65f47c5ae636cff947e946d4ff
Kevin Chen <kevinmattchen@gmail.com>
2020-02-04 22:33:37 -0800

    completed L9p2

---
+++ turnin/Lab 9/kchen161_lab9_part2.c
@@ -0,0 +1,203 @@
+/*	Author: kchen161
+ *      Partner(s) Name: Kevin Chen
+ *	Lab Section: 23
+ *	Assignment: Lab #9  Exercise 2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+#include <avr/interrupt.h>
+
+#define C 261.63
+#define D 293.66
+#define E 329.63
+#define F 349.23
+#define G 392.00
+#define A 440.00
+#define B 493.88
+
+short notes[] = {C, D, E, F, G, A, B, C*2};
+	
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
+}
+void TimerOff() {
+	TCCR1B = 0x00;
+}
+void TimerISR() {
+	TimerFlag = 1;
+}
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) {
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+void set_PWM(double frequency) {
+	static double current_frequency;
+	if (frequency != current_frequency) {
+		if (!frequency) { TCCR3B &= 0x08; }
+		else { TCCR3B |= 0x03; }
+			
+		if (frequency < 0.954) { OCR3A = 0xFFFF; }
+		else if (frequency > 31250) {OCR3A = 0x0000; }
+		else {OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
+			
+		TCNT3 = 0;
+		current_frequency = frequency;
+	}
+}
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	set_PWM(0);
+}
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
+
+enum States {start, init, wait, inc, dec, pause, play, playwait} state;
+unsigned char button;
+unsigned char ind;
+
+void tick() {
+	button = PINA & 0x07;
+	switch (state) {
+		case start:
+			state = init;
+		break;
+		
+		case init:
+			state = pause;
+		break;
+		
+		case wait:
+			if (button == 0x01) {
+				state = inc;
+				if (ind < 0x07) {
+					ind++;
+				}
+			} else if (button == 0x02) {
+				state = pause;
+			} else if (button == 0x04) {
+				state = dec;
+				if (ind > 0x00) {
+					ind--;
+				}
+			}
+		break;
+		
+		case inc:
+			if (button == 0x00) {
+				state = wait;
+			} else {
+				state = inc;
+			}
+		break;
+		
+		case dec:
+			if (button == 0x00) {
+				state = wait;
+			} else {
+				state = dec;
+			}
+		break;
+		
+		case pause:
+			if (button == 0x00) {
+				state = play;
+			} else {
+				state = pause;
+			}
+		break;
+		
+		case play:
+			if (button == 0x02) {
+				state = playwait;
+			} else {
+				state = play;
+			}
+		break;
+		
+		case playwait:
+			if (button == 0x00) {
+				state = wait;
+			} else {
+				state = playwait;
+			}
+		break;
+	}
+	
+	switch (state) {
+		case start:
+		break;
+		
+		case init:
+			set_PWM(0);
+			ind = 0;
+		break;
+		
+		case wait:
+			set_PWM(notes[ind]);
+		break;
+		
+		case inc:
+			set_PWM(notes[ind]);
+		break;
+		
+		case dec:
+			set_PWM(notes[ind]);
+		break;
+		
+		case pause:
+			set_PWM(0);
+		break;
+		
+		case play:
+		break;
+		
+		case playwait:
+		break;
+	}
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
+	
+    /* Insert your solution below */
+	TimerSet(50);
+	TimerOn();
+	
+	PWM_on();
+	
+    while (1) {
+		tick();
+		while(!TimerFlag) {}
+		TimerFlag = 0;
+    }
+    return 1;
+}
